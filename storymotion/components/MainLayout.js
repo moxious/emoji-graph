50ce57e3185032bbcb96949c9200fae6b2792b33
Moxious <moxious@oldhat.org>
2020-04-20 08:48:09 -0400

    toggle-able menu items

--- storymotion/components/MainLayout.js
+++ storymotion/components/MainLayout.js
@@ -1,39 +1,50 @@
 import Link from 'next/link';
 import { Menu } from 'semantic-ui-react'
+import React from 'react';
+import _ from 'lodash';
 
-const linkStyle = {
-    marginRight: 15
-};
+let activeItem = 'home';
 
-const MainLayout = ({ children }) => (
-    <div className='MainLayout'>
-        <Menu>
-            <Menu.Item name='home'>
-                <Link href="/">
-                    <a>Home</a>
-                </Link>
-            </Menu.Item>
+class MainLayout extends React.Component {
+    state = {
+        items: [
+            { name: 'home', link: "/", title: 'Home' },
+            { name: 'search', link: "/search", title: 'Search' },
+            { name: 'games', link: "/games", title: 'Games' },
+            { name: 'category', link: "/category", title: 'Categories' },
+        ],
+    };
 
-            <Menu.Item name='categories'>
-                <Link href="/category/">
-                    <a>Categories</a>
-                </Link>
-            </Menu.Item>
+    handleItemClick = (name) => {
+        console.log('set active', name);
+        activeItem = name;
+        return Promise.resolve(true);
+    };
 
-            <Menu.Item name='search'>
-                <Link href="/search/">
-                    <a>Search</a>
-                </Link>
-            </Menu.Item>
-        </Menu>
+    render() {
+        return (
+            <div className='MainLayout'>
+                <Menu>
+                    {
+                        this.state.items.map((i, idx) => {
+                            return (
+                                <Link href={i.link} key={idx}>
+                                    <Menu.Item
+                                        name={i.name}
+                                        active={activeItem === i.name}
+                                        onClick={() => this.handleItemClick(i.name)}>
+                                        {i.title}
+                                    </Menu.Item>
+                                </Link>
+                            );
+                        })
+                    }
+                </Menu>
 
-        <div className='MainContent'>{children}</div>
-    </div>
-);
-
-MainLayout.getInitialProps = async function (context) {
-    console.log('CONTEXT',context);
-    return {};
-};
+                <div className='MainContent'>{this.props.children}</div>
+            </div>
+        );
+    }
+}
 
 export default MainLayout;
