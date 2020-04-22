ab612eeb2a86a0243b94ba9fbef0e87e63f7d26e
Moxious <moxious@oldhat.org>
2020-04-22 11:26:25 -0400

    submitting games

--- storymotion/components/NewGame.js
+++ storymotion/components/NewGame.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import api from '../api';
-import { Loader, Grid, Header } from 'semantic-ui-react';
+import { Loader, Form, Header } from 'semantic-ui-react';
 import EmojiCompact from '../components/EmojiCompact';
+import _ from 'lodash';
 
 export default class NewGame extends React.Component {
     state = {
         pending: false,
         matrix: null,
-        x: 3,
-        y: 3
+        x: 1,
+        y: 3,
+        storyLines: [],
     };
 
-    componentWillMount() {
+    makeGame() {
         this.setState({ pending: true });
 
         return api.getMatrix(this.state.x, this.state.y)
@@ -25,6 +27,43 @@ export default class NewGame extends React.Component {
             })
             .finally(() => this.setState({ pending: false }));
     }
+
+    componentWillMount() {
+        return this.makeGame();
+    }
+
+    formComplete = () => {
+        let complete = true;
+        for (let i=0; i<this.state.x; i++) {
+            if (!this.state.storyLines[i]) { complete = false; }
+        }
+
+        return complete;
+    };
+
+    submitStory = async () => {
+        if (!this.formComplete()) {
+            console.log('Refuse to submit an incomplete form');
+        }
+
+        const data = _.pick(this.state, ['storyLines', 'matrix', 'x', 'y']);
+        console.log('SubmitStory', data);
+
+        try {
+            const result = await api.private.submitStory(data);
+            const json = await result.json();
+            console.log('Result of submitting story', json);
+        } catch (e) {
+            console.error('Error submitting story', e);
+        }
+    };
+
+    rowText = _.debounce((row, event, data) => {
+        const storyLines = this.state.storyLines;
+        _.set(storyLines, row, data.value);
+        console.log('RowText: ', row, data.value, storyLines);
+        return this.setState({ storyLines });
+    }, 200);
 
     render() {
         const wrap = children => <div className='Game'>
@@ -44,23 +83,32 @@ export default class NewGame extends React.Component {
         };
 
         return wrap(
-            <Grid rows={this.state.x} divided>
-                {
-                    this.state.matrix.map((row, i) =>
-                        <div key={`'block-${i}`}>
-                            <Grid.Row key={i}>
-                                {
+            <div className='NewGame'>
+                <Form>
+                    {
+                        this.state.matrix.map((row, i) =>
+                            <div key={`row-${i}`}>
+                                <p style={cellStyle}>{
                                     this.state.matrix[i].map((col, j) =>
-                                        <Grid.Column style={cellStyle} key={`${i}_${j}`}>
-                                            <EmojiCompact data={this.state.matrix[i][j]} />
-                                        </Grid.Column>
+                                        <span key={`span${j}`}><EmojiCompact data={this.state.matrix[i][j]} />&nbsp;</span>
                                     )
-                                }
-                            </Grid.Row>
-                        </div>
-                    )
-                }
-            </Grid>
+                                }</p>
+
+                                <Form.Group style={cellStyle}>
+                                    <Form.Input
+                                        maxLength={1000}
+                                        onChange={(e, d) => this.rowText(i, e, d)}
+                                        placeholder='story' />
+                                    <Form.Button type='submit' disabled={!this.formComplete()}
+                                        onClick={this.submitStory}>
+                                            Submit
+                                    </Form.Button>
+                                </Form.Group>
+                            </div>
+                        )
+                    }
+                </Form>
+            </div>
         );
     }
 }
