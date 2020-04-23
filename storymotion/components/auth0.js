ff173a8bbb70501b848ee86e117822085614adac
Moxious <moxious@oldhat.org>
2020-04-23 08:12:30 -0400

    remove redundant

--- storymotion/components/auth0.js
+++ storymotion/components/auth0.js
@@ -11,7 +11,4 @@ export default () => ({
     AUTH0_CLIENT_ID: 'vGKmSRL3jNlfPHxZedvE6W3H3bKNPAJO',
     AUTH0_DOMAIN: 'dev-iey-o5gl.auth0.com',
     AUTH0_CALLBACK_URL: 'http://localhost:3000/',
-    apiOptions: {
-
-    }
 });
